cda49fcce31275a9e32be782cf560e9913103bfb
fabripetrelli <fabrizio.petrelli@brainsen.com>
2021-05-12 15:31:29 +0200

    Greeting Modal structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 
+import GreetingModal from './components/Modal'
+
 const App: React.FC = () => {
-  const greetings = 'Learn React'
+  const greetings = 'Welcome'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          {greetings}
-        </a>
-      </header>
+    <div className="bgcolor">
+      <div>{greetings}</div>
+      <GreetingModal />
     </div>
   )
 }
-
 export default App
---
+++ src/components/Modal.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+
+// Chackra UI
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  Button,
+  Image
+} from '@chakra-ui/react'
+
+// Style
+import './modal.css'
+
+// Assets
+import fabripetrelli from '../images/fabriziopaolopetrelli.jpg'
+
+const GreetingModal: React.FC = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(true)
+  return (
+    <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} isCentered size="3xl" motionPreset="slideInBottom">
+      <ModalContent>
+        <ModalHeader>
+          <ModalCloseButton />
+        </ModalHeader>
+        <ModalBody className="modalBody">
+          <div className="breathc">
+            <Image
+              borderRadius="full"
+              boxSize="150px"
+              alt="Fabrizio Paolo Petrelli"
+              src={fabripetrelli}
+              className="modalImg"
+            />
+          </div>
+          <div className="modalContentText">
+            <h1>FABRIZIO PAOLO PETRELLI</h1>
+            Hi, I&apos;m Fabrizio
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <Button onClick={() => setIsOpen(false)}>Go deeper</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  )
+}
+export default GreetingModal
